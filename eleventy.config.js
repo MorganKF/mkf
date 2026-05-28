5cec2870b5eacec4a2d92783dfa8df9aa17ecf2d
Morgan Fudge <morgan@mkf.dev>
2026-05-28 04:15:12 -0230

    Update website design

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 import postcss from "postcss";
 import autoprefixer from "autoprefixer";
+import postcssImport from "postcss-import";
+import cssnano from "cssnano";
 import syntaxHighlighting from "@11ty/eleventy-plugin-syntaxhighlight";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import markdownIt from "markdown-it";
@@ -13,8 +15,16 @@ export default function (eleventyConfig) {
   eleventyConfig.setIncludesDirectory("_includes");
   eleventyConfig.setLayoutsDirectory("_layouts");
 
+  eleventyConfig.ignores.add("src/_styles/**");
+
   // Add public folder
   eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Add icons
+  eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
+  eleventyConfig.addPassthroughCopy({
+    "src/site.webmanifest": "site.webmanifest",
+  });
 
   // Export sanitize css
   eleventyConfig.addPassthroughCopy({
@@ -34,9 +44,16 @@ export default function (eleventyConfig) {
   eleventyConfig.addTemplateFormats("css");
   eleventyConfig.addExtension("css", {
     outputFileExtension: "css",
+    compileOptions: {
+      cache: false,
+    },
     compile: async (inputContent, inputPath) => {
       try {
-        const result = await postcss([autoprefixer]).process(inputContent, {
+        const result = await postcss([
+          postcssImport,
+          autoprefixer,
+          cssnano,
+        ]).process(inputContent, {
           from: inputPath,
         });
         return async () => result.css;
@@ -44,6 +61,16 @@ export default function (eleventyConfig) {
         console.error("PostCSS error:", err);
       }
     },
+  });
+
+  // Add read time calculation
+  eleventyConfig.addFilter("readTime", (content) => {
+    const wordsPerMinute = 200;
+    const textOnly = content.replace(/(<([^>]+)>)/gi, "");
+    const wordCount = textOnly.split(/\s+/).length;
+    const minutes = Math.ceil(wordCount / wordsPerMinute);
+
+    return minutes === 1 ? "1 min read" : `${minutes} min read`;
   });
 
   // Add plugins
